000415546d6f6b4a00749e6706111d6afe44dbab
github username <github email address>
2018-09-15 01:38:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
 // Code your solution here:
+//Question 1
+// V1
+// function driversWithRevenueOver(drivers, revenue){
+//   var newDrivers = [];
+//   for (const driver of drivers){
+//     if (driver.revenue > Number(revenue)){
+//         newDrivers.push(driver);
+//     }
+//   }
+//   return newDrivers;
+// }
+// Final Version
 function driversWithRevenueOver(drivers, revenue){
-  var newDrivers = [];
-  for (const driver of drivers){
-    if (driver.revenue > Number(revenue)){
-        newDrivers.push(driver);
-    }
-  }
-  return newDrivers;
+  return drivers.filter(driver => driver.revenue > Number(revenue));
 }
 
-
+//Question 2
+// V1
+// function driverNamesWithRevenueOver(drivers, revenue){
+//   var newDrivers = [];
+//   for (const driver of drivers){
+//     if (driver.revenue > Number(revenue)){
+//       newDrivers.push(driver.name);
+//     }
+//   }
+//   return newDrivers;
+// }
+// Final Version
 function driverNamesWithRevenueOver(drivers, revenue){
-  var newDrivers = [];
-  for (const driver of drivers){
-    if (driver.revenue > Number(revenue)){
-      newDrivers.push(driver.name);
-    }
-  }
-  return newDrivers;
+  return drivers.filter(driver => driver.revenue > Number(revenue)).map(driver => driver.name);
 }
 
 
+//Question 3
+//V1
+// function exactMatch(drivers, input){
+//   var newDrivers = [];
+//   for (const driver of drivers){
+//     if (driver.name === input.name || driver.revenue === Number(input.revenue)){
+//       newDrivers.push(driver);
+//     }
+//   }
+//   return newDrivers;
+// }
+// Final Version
 function exactMatch(drivers, input){
-  var newDrivers = [];
-  for (const driver of drivers){
-    if (driver.name === input.name || driver.revenue === Number(input.revenue)){
-      newDrivers.push(driver);
-    }
-  }
-  return newDrivers;
+  return drivers.filter(function(driver) {
+    return driver.name === input.name || driver.revenue === Number(input.revenue);
+  })
 }
 
+
+//Question 4
+//V1
+// function exactMatchToList(drivers, input){
+//   var newDrivers = [];
+//   for (const driver of drivers){
+//     if (driver.name === input.name || driver.revenue === Number(input.revenue)){
+//       newDrivers.push(driver.name);
+//     }
+//   }
+//   return newDrivers;
+// }
+// Final Version
 function exactMatchToList(drivers, input){
-  var newDrivers = [];
-  for (const driver of drivers){
-    if (driver.name === input.name || driver.revenue === Number(input.revenue)){
-      newDrivers.push(driver.name);
-    }
-  }
-  return newDrivers;
+  return exactMatch(drivers, input).map(driver => driver.name);
 }
